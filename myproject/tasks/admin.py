b518359297db762b8261bb0baca5fb953b6cb12e
Grimm-Z <zakharmazno@gmail.com>
2024-08-22 13:07:24 +0300

    Update admin.py

--- myproject/tasks/admin.py
+++ myproject/tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Task
+# Register your models here.@admin.register(Task)
+@admin.register(Task)
+class TakeAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "assigned_to")
+    list_filter = ("status", "assigned_to")
+    search_fields = ("status", "assigned_to")
